f44903c1536d5ce83ab182c70f0f18292c02cfc6
patikpe <patrickpdev@pm.me>
2024-04-24 17:05:55 +0200

    firebase options added

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_playground/app/app.dart';
 import 'package:flutter_playground/app/app_logger.dart';
 import 'package:flutter_playground/app/app_observer.dart';
+import 'package:flutter_playground/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   AppLogger.instance;
   Bloc.observer = AppObserver();
   runApp(const MyApp());
