c7be990d619edefece57b1af2afe9c15c019c201
patikpe <patrickpdev@pm.me>
2024-04-28 21:46:29 +0200

    stuff

--- lib/modules/setup/cubit/setup_cubit.dart
+++ lib/modules/setup/cubit/setup_cubit.dart
@@ -7,12 +7,6 @@ class SetupCubit extends Cubit<SetupState> {
   SetupCubit() : super(const SetupState());
 
   void redirectLoginScreen() {
-    emit(state.copyWith(status: SetupStatus.loading));
-    Future.delayed(const Duration(seconds: 15),
-        () => emit(state.copyWith(status: SetupStatus.redirectLoginScreen)));
-  }
-
-  void redirectHomeScreen() {
-    emit(state.copyWith(status: SetupStatus.redirectHomeScreen));
+    emit(state.copyWith(status: SetupStatus.redirectLoginScreen));
   }
 }
--- lib/modules/setup/setup_view.dart
+++ lib/modules/setup/setup_view.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_playground/modules/setup/cubit/setup_cubit.dart';
 import 'package:go_router/go_router.dart';
 
-class SetupView extends StatelessWidget {
+class SetupView extends StatefulWidget {
   const SetupView({super.key});
 
   @override
+  State<SetupView> createState() => _SetupViewState();
+}
+
+class _SetupViewState extends State<SetupView> {
+  @override
   Widget build(BuildContext context) {
-    return BlocListener<SetupCubit, SetupState>(
-      bloc: SetupCubit()..redirectLoginScreen(),
-      listener: (context, state) {
-        if (state.status == SetupStatus.redirectLoginScreen) {
-          context.go("/login");
-        }
-      },
-      child: const Placeholder(),
+    return BlocProvider(
+      create: (context) => SetupCubit(),
+      child: BlocListener<SetupCubit, SetupState>(
+        listener: (context, state) {
+          if (state.status == SetupStatus.redirectLoginScreen) {
+            context.go('/login');
+          }
+        },
+        child: Builder(builder: (context) {
+          SchedulerBinding.instance.addPostFrameCallback((_) {
+            context.read<SetupCubit>().redirectLoginScreen();
+          });
+          return const Placeholder();
+        }),
+      ),
     );
   }
 }
