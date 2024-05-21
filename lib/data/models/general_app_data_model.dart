b3b1319a8db94ba0139162d5132a82f395d25563
patikpe <patrickpdev@pm.me>
2024-05-21 21:47:02 +0200

    removed object box

--- lib/data/models/general_app_data_model.dart
+++ lib/data/models/general_app_data_model.dart
@@ -1,10 +1,4 @@
-import 'package:objectbox/objectbox.dart';
-
-@Entity()
 class GeneralAppDataModel {
-  @Id(assignable: true)
-  int id = 0;
-
   String? name;
 
   GeneralAppDataModel({
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -5,7 +5,6 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   firebase_auth
   firebase_core
-  objectbox_flutter_libs
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
