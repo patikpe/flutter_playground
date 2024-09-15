11fd934802c4a6d64ce30da3eb287e59d3d60f1d
patikpe <patrickpdev@pm.me>
2024-09-15 17:56:31 +0200

    iyuh

--- lib/utilities/form_values_enum.dart
+++
@@ -1,9 +0,0 @@
-enum FormEnumValues {
-  email(code: 'email'),
-  password(code: 'password'),
-  passwordConfirm(code: 'passwordConfirm');
-
-  const FormEnumValues({required this.code});
-
-  final String code;
-}
